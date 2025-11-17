23eaf2a798b1fd11b3d0200248a5ed9a00f495bc
ioan.albuica <ioan.albuica@stud.acs.upb.ro>
2025-11-17 19:22:11 +0200

    firebase_setup v3 + db_user v0

---
+++ db_user.py
@@ -0,0 +1,7 @@
+import firebase_admin
+from firebase_admin import firestore
+from firebase_setup import db
+
+def add_account(email, name, password):
+    
+    return None
--- firebase_setup.py
+++ firebase_setup.py
@@ -1,11 +1,13 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
+db = None
+
 def get_database():
 
     if not firebase_admin._apps:
         cred = credentials.Certificate("/home/ia4/pennywise-9367d-firebase-adminsdk-fbsvc-dccb030fe4.json")
         firebase_admin.initialize_app(cred)
 
-    return firestore.client()
+    db = firestore.client()
 
